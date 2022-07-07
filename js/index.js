b98bee80f1092c183155db5312744d8fc7ca1ce9
DanFerrari <danieldasilvaferrari@gmail.com>
2022-07-06 23:05:39 -0300

    add files 🌱

---
+++ js/index.js
@@ -0,0 +1,6 @@
+//classe que vai conter a lógica dos dados 
+//como os dados serão estruturados
+//classe que vai criar a visualização e eventos do HTML
+import { FavoritesView } from "./favorites.js";
+
+new FavoritesView('body')
